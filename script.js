14f38a8be8ff3e1b412c257381077d33a207a0c7
nixinsider <nixinsider@gmail.com>
2025-08-14 23:46:02 +0500

    side bar done

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {
-    $('#dropdownBtn').on('click', function() {
-        setTimeout(function() {
-        $('#myDropdown').toggleClass('hide');
-        }, 100);
+$(document).ready(function () {
+    $('.dropbtn').on('click', function () {
+        let dropdown = $(this).parent().next('.dropdown-content'); 
+        dropdown.toggleClass('hide');
         $(this).toggleClass('rotate');
     });
 });
+function checkDropdown(){
+    dropdown.forEach(dropdown => {
+        if (!dropdown.classList.contains('hide')) {
+            dropdown.classList.add('hide');
+            dropdown.previousElementSibling.classList.remove('rotate');
+        }
+    });
+}
